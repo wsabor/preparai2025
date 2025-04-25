ebdc15eee9239eff65537161576ada10ea6987f2
Wagner Sabor <wsaborjr@gmail.com>
2025-04-25 15:21:37 -0300

    final do Sprint 1

--- backend/server.js
+++ backend/server.js
@@ -26,5 +26,5 @@ app.get("/api/perguntas", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
