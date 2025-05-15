f660bd8d3175c1c037064c3c43ba53803eeae6e1
Wagner Sabor <wsaborjr@gmail.com>
2025-05-15 19:29:11 -0300

    ajustes gerais de estilização, cores, margens e botões

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../styles/Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <span>Desenvolvido por: Professor Wagner Sabor</span>
+      <span>Desenvolvido por: Prof. Wagner Sabor</span>
       <span>Versão 1.0.0</span>
       <span>&copy; 2025 Quiz Prepara Aí</span>
     </footer>
