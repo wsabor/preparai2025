08b3ed25625c60962f4bd1b6fdbb8842b06ca18d
Wagner Sabor <wsaborjr@gmail.com>
2025-05-16 08:15:44 -0300

    ajustes de estilização, margens e clean code

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,9 +3,11 @@ import "../styles/Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <span>Desenvolvido por: Prof. Wagner Sabor</span>
-      <span>Versão 1.0.0</span>
-      <span>&copy; 2025 Quiz Prepara Aí</span>
+      <div className="container">
+        <span>Desenvolvido por: Prof. Wagner Sabor</span>
+        <span>Versão 1.0</span>
+        <span>&copy; 2025 Quiz Prepara Aí</span>
+      </div>
     </footer>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -18,20 +18,22 @@ export default function Header() {
 
   return (
     <header className="header">
-      <div className="logo">
-        <img src={logoQuiz} alt="Logo do Quiz Prepara Aí" />
-        <h1>Quiz Prepara Aí 2025</h1>
-      </div>
-      <div className="buttons">
-        <Link to="/home">
-          <button className="btn-primary">Início</button>
-        </Link>
-        <Link to="/ranking">
-          <button className="btn-secondary">Ver Ranking</button>
-        </Link>
-        <button onClick={handleLogout} className="btn-logout">
-          Sair
-        </button>
+      <div className="container">
+        <div className="logo">
+          <img src={logoQuiz} alt="Logo do Quiz Prepara Aí" />
+          <h1>Quiz Prepara Aí 2025</h1>
+        </div>
+        <div className="buttons">
+          <Link to="/home">
+            <button className="btn-primary">Início</button>
+          </Link>
+          <Link to="/ranking">
+            <button className="btn-secondary">Ver Ranking</button>
+          </Link>
+          <button onClick={handleLogout} className="btn-logout">
+            Sair
+          </button>
+        </div>
       </div>
     </header>
   );
