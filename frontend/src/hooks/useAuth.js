dffbf2252aa814c45e99c1076defb36bf089200b
Wagner Sabor <wsaborjr@gmail.com>
2025-06-06 13:08:16 -0300

    ajuste hook useAuth.js

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -9,7 +9,7 @@ import {
   updateProfile,
   signOut,
 } from "firebase/auth";
-import { auth, googleProvider } from "../firebaseConfig";
+import { auth, googleProvider } from "../firebaseConfig.js";
 
 // Função helper para traduzir códigos de erro do Firebase (opcional, mas útil)
 const getFirebaseAuthErrorMessage = (errorCode) => {
